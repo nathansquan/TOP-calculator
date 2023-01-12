72a3dda279fc61c423eb242bbe26f5fd5147483e
Nathan Quan <nathansquan@gmail.com>
2023-01-12 00:01:38 -0500

    refactor html. Basic calculator operations work. TODO: Clear button.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,10 @@
+let displayValue = '0';
+let firstOperand = null;
+let secondOperand = null;
+let firstOperator = null;
+let secondOperator = null;
+let result = null;
+
 function add(a, b) {
     return a + b;
 }
@@ -25,23 +32,123 @@ function operate(operator, a, b) {
         return subtract(a, b);
     } else if (operator === '*') {
         return multiply(a, b);
-    } else {
+    } else if (operator === '/') {
         return divide(a, b);
     }
 }
 
-// create functions to display number button clicks
+// watch for clicks of digits and decimal
 const numBtns = document.querySelectorAll(".digits > button");
-const display = document.querySelector(".display");
-
-// initialize displayVal var to store number that is created through clicks
-let displayVal;
-
 numBtns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
-        const val = e.target.textContent;
-        display.textContent = `${display.textContent}${val}`;
-        // update displayVal after each number button click
-        displayVal = display.textContent;
+        const num = e.target.textContent;
+        inputOperand(num);
+        updateDisplay();
     });
 });
+
+// watch for clicks of operands
+const operandBtns = document.querySelectorAll(".operators > button:not(#equals)");
+operandBtns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        const operator = e.target.name;
+        inputOperator(operator);
+        updateDisplay();
+    });
+});
+
+// watch for clicks of equals button
+const equalsBtn = document.querySelector("#equals");
+equalsBtn.addEventListener('click', () => {
+    inputEquals();
+    updateDisplay();
+});
+
+// updateDisplay function
+// updates display on click
+function updateDisplay() {
+    const display = document.querySelector(".display");
+    display.textContent = displayValue;
+}
+
+updateDisplay();
+
+
+// inputOperand takes an operand as input and prints it on display
+function inputOperand(operand) {
+    // case when first operator hasn't been inputted yet
+    if (firstOperator === null) {
+        if (displayValue === '0') {
+            displayValue = operand;
+        } else {
+            displayValue += operand;
+        }
+    } else {
+        // case when the first operator has been clicked
+        if (displayValue === firstOperand) {
+            displayValue = operand;
+        } else {
+            displayValue += operand;
+        }
+    }
+}
+
+
+function inputOperator(operator) {
+    if (firstOperator === null && secondOperator === null) {
+        // case when an operator is clicked for the very first time
+        firstOperator = operator;
+        firstOperand = displayValue;
+    } else if (firstOperator !== null && secondOperator === null) {
+        // case when a second operator is clicked for the first time
+        secondOperator = operator;
+        secondOperand = displayValue;
+        result = operate(firstOperator, Number(firstOperand), Number(secondOperand));
+        displayValue = result.toString();
+        // reset firstOperand to the result/display value to be ready for
+        // another operator click
+        firstOperand = displayValue;
+        // reset result
+        result = null;
+    } else {
+        // case when a new second operator is clicked i.e. the 6th click
+        secondOperator = operator;
+        secondOperand = displayValue;
+        result = operate(secondOperator, Number(firstOperand), Number(secondOperand));
+        displayValue = result.toString();
+        // reset firstOperand to the result/display value to be ready for
+        // another operator click
+        firstOperand = displayValue;
+        // reset result
+        result = null;
+    }
+
+}
+
+function inputEquals() {
+    // clicking equals with no firstOperator will not cause an error
+    if (firstOperator === null) {
+        displayValue = displayValue;
+    } else if (firstOperator !== null && secondOperator === null) {
+        // case when equals button clicked for the very first time
+        secondOperand = displayValue;
+        result = operate(firstOperator, Number(firstOperand), Number(secondOperand));
+        displayValue = result.toString();
+        secondOperand = null;
+        firstOperator = null;
+        secondOperator = null;
+        result = null;
+    } else if (secondOperator !== null) {
+        // case when final result
+        // case when equals button clicked after two operator clicks and an
+        // operand click
+        secondOperand = displayValue;
+        result = operate(secondOperator, Number(firstOperand), Number(secondOperand));
+        displayValue = result.toString();
+        firstOperand = displayValue;
+        secondOperand = null;
+        firstOperator = null;
+        secondOperator = null;
+        result = null;
+    }
+}
